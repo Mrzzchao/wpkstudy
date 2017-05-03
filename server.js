d39b8c57fe226d7d1a4eddb6088c1b6d4479815b
sampsonli <740056710@qq.com>
2017-05-03 21:30:54 +0800

    fix bug

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
 /**
  * Created by lichun on 2017/1/19.
  */
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const path = require('path')
 
-const webpackDevMiddleware = require("webpack-dev-middleware");
-const webpackHotMiddleware = require('webpack-hot-middleware');
-const webpack = require("webpack");
-const webpackConfig = require("./build/webpack.dev.config");
+const webpackDevMiddleware = require('webpack-dev-middleware')
+const webpackHotMiddleware = require('webpack-hot-middleware')
+const webpack = require('webpack')
+const webpackConfig = require('./build/webpack.dev.config')
 
-
-const app = express();
-if(process.env.NODE_ENV==='production'){
-    const compression = require('compression');
-    app.use(compression());
-    app.use(express.static(path.join(__dirname, 'dist'), {
-        maxAge: '1d'
-    }));
-}else {
-    const compiler = webpack(webpackConfig);
-    app.use(webpackDevMiddleware(compiler, {
-        hot: true,
-        publicPath: webpackConfig.output.publicPath
-    }));
-    app.use(webpackHotMiddleware(compiler, {
-        log: ()=>{}
-    }));
+const app = express()
+if (process.env.NODE_ENV === 'production') {
+  const compression = require('compression')
+  app.use(compression())
+  app.use(express.static(path.join(__dirname, 'dist'), {
+    maxAge: '1d'
+  }))
+} else {
+  const compiler = webpack(webpackConfig)
+  app.use(webpackDevMiddleware(compiler, {
+    hot: true,
+    quiet: true,
+    publicPath: webpackConfig.output.publicPath
+  }))
+  app.use(webpackHotMiddleware(compiler, {
+    log: () => {}
+  }))
 }
 
 app.listen(8080, function () {
-    console.log("Listening on port 8080!");
-});
+  console.log('Listening on port 8080!')
+})
